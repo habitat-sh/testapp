a8b5bc95375f0417ab5040fb6c1508dc55ce0027
Josh Black <raskchanky@gmail.com>
2017-12-20 21:45:29 +0000

    MOAR AWESOME

---
+++ awesome/plan.sh
@@ -0,0 +1,29 @@
+pkg_origin=neurosis
+pkg_name=testapp
+pkg_version=0.1.3
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=()
+pkg_upstream_url=https://github.com/habitat-sh/habitat-example-plans
+pkg_source=nosuchfile.tar.gz
+pkg_deps=()
+pkg_expose=()
+
+do_download() {
+  return 0
+}
+
+do_verify() {
+  return 0
+}
+
+do_unpack() {
+  return 0
+}
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  return 0
+}
